c553e511600b355ffba61c70636ee4509a0bca1b
Kamala <kamala.abdulova1@gmail.com>
2023-10-10 00:36:59 -0700

    i

--- AutomationProject.java
+++
@@ -1,6 +0,0 @@
-public class AutomationProject {
-    public static void main(String[] args) {
-        WebDriver driver = new GoogleChrome
-                
-    }
-}
---
+++ src/test/java/AutomationProject.java
@@ -0,0 +1,9 @@
+import org.openqa.selenium.WebDriver;
+
+public class AutomationProject {
+    public static void main(String[] args) {
+        WebDriver driver = new GoogleChrome();
+    }
+}
+
+
